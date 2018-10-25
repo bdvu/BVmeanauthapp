858a80b47b0540a356c09f55a5d502db90906680
bdvu <44455997+bdvu@users.noreply.github.com>
2018-10-24 22:39:21 -0700

    Updating app.js

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+// Connect To Database
 mongoose.connect(config.database, {useNewUrlParser: true});
 
+// On Connection
 mongoose.connection.on('connected', () => {
    console.log('Connected to database '+config.database);
+});
+
+// On Error
+mongoose.connection.on('error', (err) => {
+   console.log('Database error: '+err);
 });
 
 const app = express();
